3450017acb6fd820b9a3074332a3473a66c35b83
Vicktor <133223399+vicktorviggo@users.noreply.github.com>
2025-06-27 16:22:11 +0200

    added escrow_ignore for tebex version

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,9 +1,15 @@
 fx_version 'cerulean'
+lua54 'yes'
 game 'gta5'
 
 author 'vicktorviggo'
 version '1.0.0'
 
 client_scripts {
+    'client.lua'
+}
+
+escrow_ignore {
+    'fxmanifest.lua',
     'client.lua'
 }
